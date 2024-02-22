ce92c202512a627ef1f856fe1d32a651f26ba151
KiddoKishore <kiddokishore@gmail.com>
2024-02-22 22:34:41 +0530

    Make the Title Responsive

--- Frontend/src/screens/HomeScreen.jsx
+++ Frontend/src/screens/HomeScreen.jsx
@@ -5,7 +5,7 @@ import Trips from '../components/Trips';
 const HomeScreen = () => {
   return (
     <div>
-      <h1 className='text-center text-cyan-50/75 text-5xl p-5'>Welcome To Black Bus</h1>
+      <h1 className='text-center text-cyan-50/75 md:text-5xl text-xl p-5'>Welcome To Black Bus</h1>
       <Search />
       <div className='bg-white mb-20'>
         <Trips />
