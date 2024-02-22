7bbef79672ffae53416bcfb4586f4b039cf968c9
KiddoKishore <kiddokishore@gmail.com>
2024-02-22 22:36:23 +0530

    Style the Expired Tickets

--- Frontend/src/screens/MyTicketsScreen.jsx
+++ Frontend/src/screens/MyTicketsScreen.jsx
@@ -19,11 +19,11 @@ const MyTicketsScreen = () => {
     const reversedTickets = tickets ? [...tickets].reverse() : [];
 
     return (
-        <div className='flex flex-wrap'>
+        <div className='flex flex-wrap justify-center'>
             {
                 reversedTickets.map((ticket) => (
-                    <Card key={ticket._id} className={`m-5 w-96 p-3 ${ ticket.isBooked ? 'bg-green-400/75' : 'bg-red-400/75'}`}>
-                        <div className='flex justify-between'>
+                    <Card key={ticket._id} className={`m-5 w-96 p-3 ${ ticket.isBooked ? ticket.date.slice(0, 10) < new Date().toISOString().slice(0, 10) ? 'bg-yellow-400/75' : 'bg-green-400/75' : 'bg-red-400/75'} ${ ticket.date < new Date() && 'bg-yellow-400'}`}>
+                        <div className='flex justify-between p-2'>
                             <span><h2>{ticket.origin}</h2><h3>{ticket.departureTime}</h3></span>
                             <span><h1>{ticket.busNumber}</h1><h2>{ticket.date.slice(0, 10)}</h2></span>
                             <span><h2>{ticket.destination}</h2><h3>{ticket.arrivalTime}</h3></span>
@@ -31,24 +31,24 @@ const MyTicketsScreen = () => {
                             <table>
                                 <thead>
                                     <tr>
-                                        <th>Name</th>
-                                        <th>Age</th>
-                                        <th>Seat No</th>
+                                        <th className='px-1'>Name</th>
+                                        <th className='px-1'>Age</th>
+                                        <th className='px-1'>Seat No</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {
                                         ticket.passengers.map((passenger, index) => (
                                             <tr key={index}>
-                                                <td>{passenger.name}</td>
-                                                <td>{passenger.age}</td>
-                                                <td>{passenger.seatNo}</td>
+                                                <td className='px-1'>{passenger.name}</td>
+                                                <td className='px-1'>{passenger.age}</td>
+                                                <td className='px-1'>{passenger.seatNo}</td>
                                             </tr>
                                         ))
                                     }
                                 </tbody>
                             </table>
-                            <h1>Total Price : {ticket.totalPrice}</h1>
+                            <h1 className='p-1'>Total Price : {ticket.totalPrice}</h1>
                             <Link to={`/ticket/${ticket._id}`}>
                                 <Button type='button' onClick={() => handleViewTicket(ticket)}>View Full Ticket</Button>
                             </Link>
