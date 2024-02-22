8fdce3aaef00d1309aad49909717e716c7939199
KiddoKishore <kiddokishore@gmail.com>
2024-02-22 22:27:04 +0530

    Set Footer in the Bottom of the Web Page

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     const currentYear = new Date().getFullYear();
   return (
-    <div className='grid content-end text-center bottom-0 mb-0 w-full backdrop-blur text-white p-5 mt-5' style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+    <div className='text-center bottom-0 mb-0 w-full backdrop-blur text-white p-5 mt-5' style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
         <p>BlackBus &copy; {currentYear}</p>
     </div>
   )
