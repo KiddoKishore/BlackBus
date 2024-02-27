adc1236bdf3e9137fbc1520dde6ac8b70062a692
KiddoKishore <kiddokishore@gmail.com>
2024-02-27 10:30:18 +0530

    Align the Footer Div

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     const currentYear = new Date().getFullYear();
   return (
-    <div className='text-center bottom-0 mb-0 w-full backdrop-blur text-white p-5 mt-5' style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+    <div className='absolute text-center bottom-0 mb-auto w-full backdrop-blur text-white p-5 mt-5' style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
         <p>BlackBus &copy; {currentYear}</p>
     </div>
   )
